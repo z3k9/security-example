4168126c06a06ac08a235bdfd8851f2eb04c4fbe
z3k9 <ajelelade@gmail.com>
2022-11-18 12:14:25 +0100

    nothing much

--- server.js
+++ server.js
@@ -2,19 +2,35 @@ const https= require('https');
 
 const path = require('path');
 
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
 
-const fs = require('fs');
+const helmet = require('helmet');
 
 const PORT = 3000;
+
+app.use('helmet');
+
+function checkLoggedIn(req, res, next){
+    const isLoggedIn = true;
+    if(!isLoggedIn){
+        return res.status(401).json({
+            error: 'You must be logged in'
+        });
+    }
+    next();
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/secret', (req,res)=>{
+app.get('/secret',checkLoggedIn, (req,res)=>{
     return res.send('Your personal secret value is 42!');
 })
 
